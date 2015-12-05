dcb0b623252094e137794ad1ce60ed5a49fee293
Netanel Gilad <netanelgilad@gmail.com>
2015-12-05 10:12:57 +0200

    rename package to minimize confusion

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
-  name: 'angular:angular-server',
+  name: 'netanelgilad:polyfill-angular-server',
   version: '1.4.4',
-  // Brief, one-line summary of the package.
   summary: 'Get AngularJS on the server side!',
-  // URL to the Git repository containing the source code for this package.
   git: 'http://gihub.com/netanelgilad/meteor-angular-server',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
